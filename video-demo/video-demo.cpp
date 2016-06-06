0ffd40f18af559e83868e5037e873c5a264abb73
Abdelrahman Mahmoud <mahmoud@affectiva.com>
2016-06-05 23:52:17 -0400

    Fix location of highgui header

--- video-demo/video-demo.cpp
+++ video-demo/video-demo.cpp
@@ -3,7 +3,7 @@
 #include <chrono>
 #include <fstream>
 
-#include <highgui.h>
+#include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <boost/filesystem.hpp>
 #include <boost/timer/timer.hpp>
